05fc54696308758652aee82dbe84c42ca8df4389
Hernan Vallejos <vallejoshm@gmail.com>
2022-06-12 17:11:25 -0300

    prueba heroku

--- src/main/java/com/apirest/argentinaprograma/backend/controller/Controller.java
+++ src/main/java/com/apirest/argentinaprograma/backend/controller/Controller.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin( origins ="*")
+@CrossOrigin(origins = "https://portfolio-web-hmv.herokuapp.com")
 @Transactional
 @RestController
 @RequestMapping("/")
--- src/main/java/com/apirest/argentinaprograma/backend/security/MainSecurity.java
+++ src/main/java/com/apirest/argentinaprograma/backend/security/MainSecurity.java
@@ -19,7 +19,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin
+@CrossOrigin(origins = "https://portfolio-web-hmv.herokuapp.com")
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
