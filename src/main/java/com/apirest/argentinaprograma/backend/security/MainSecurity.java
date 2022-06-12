989135fb7eb18a2fd0562f608245984377915627
Hernan Vallejos <vallejoshm@gmail.com>
2022-06-12 16:38:17 -0300

    prueba heroku con base en clev

--- src/main/java/com/apirest/argentinaprograma/backend/security/MainSecurity.java
+++ src/main/java/com/apirest/argentinaprograma/backend/security/MainSecurity.java
@@ -61,7 +61,7 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.cors().and().csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/auth/**").permitAll()
+                .antMatchers("https://fast-brook-89046.herokuapp.com/auth/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
