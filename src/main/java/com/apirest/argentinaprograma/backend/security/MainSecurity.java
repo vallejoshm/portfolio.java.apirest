476a1cf5944fbd4598416a14c422fd2e0ffb4474
Hernan Vallejos <vallejoshm@gmail.com>
2022-06-12 16:33:07 -0300

    prueba heroku con base en clever

--- src/main/java/com/apirest/argentinaprograma/backend/security/MainSecurity.java
+++ src/main/java/com/apirest/argentinaprograma/backend/security/MainSecurity.java
@@ -17,7 +17,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
